6d1c8e3568a0db89eaffcde841bdcc13830464cb
LakshmiChaitanya2008 <lakshmichaitanyachalamalasetti@gmail.com>
2026-05-12 19:24:13 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -8,6 +9,9 @@ int main(int argc, char *argv[]) {
     printf("$ ");
     char cmd[100]; 
     scanf("%s", cmd);
+    if(strcmp(cmd, "exit") == 0) {
+	exit(0);
+    }
     printf("%s: command not found\n", cmd);
   }
   return 0;
